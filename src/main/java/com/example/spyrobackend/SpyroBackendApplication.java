ba9512810d6e5c75b4cf9640b31d2297569a1859
manuela <marmanuelam@gmail.com>
2026-06-12 21:25:08 +0200

    Initial commit

---
+++ src/main/java/com/example/spyrobackend/SpyroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.spyrobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpyroBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spyrobackend/SpyroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spyrobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpyroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
